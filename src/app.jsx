578e42a4d7922150e2f6a9901cf46c2a85c90172
BVBFD <lsevina126@gmail.com>
2021-08-19 14:29:41 +0900

    Code Search Engine and Enscript API_KEY by .env

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,9 @@ function App() {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  // env 활용한 api키 보안책은 터미널 종료하고 다시 켜야 적용 가능
+
   const selectVideo = (video) => {
     console.log(video);
     setSelectedVideo(video);
@@ -19,7 +22,7 @@ function App() {
       redirect: 'follow'
     };
     
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC-iohmFKEU3z2hf5GLtys-AjFhXQf2whk`, requestOptions)
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${API_KEY}`, requestOptions)
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
@@ -31,7 +34,7 @@ function App() {
       redirect: 'follow'
     };
     
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=25&regionCode=KR&key=AIzaSyC-iohmFKEU3z2hf5GLtys-AjFhXQf2whk", requestOptions)
+    fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=25&regionCode=KR&key=${API_KEY}`, requestOptions)
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
